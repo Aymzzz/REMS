b170da1a89468bf9dde2679593b9da0e73daa7be
Aymzzz <sbaiaymane6@gmail.com>
2024-04-07 03:16:18 +0000

    Added comments

--- src/realestate/Comment.java
+++ src/realestate/Comment.java
@@ -1,5 +1,17 @@
 package realestate;
 
-public class Comment {
+import java.util.List;
 
+public class Comment {
+    private int commentID;
+    private int authorID;
+    private int propertyID;
+    private String content;
+    private List<Comment> replies;
+
+    public void postComment(String content) {
+    }
+
+    public void reply(Comment comment, String content) {
+    }
 }
