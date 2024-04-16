4488215ffb6b04b19934a54d56404771cebde476
Badr-ech <166541769+Badr-ech@users.noreply.github.com>
2024-04-16 12:19:25 +0100

    Update Studio.java
    added constructor
    removed location and studio name attributes

--- src/realestate/Studio.java
+++ src/realestate/Studio.java
@@ -2,26 +2,16 @@ package realestate;
 
 public class Studio{
     private int studioID;
-    private String location;
-    private String studioName;
     private String studioType;
+    public Studio(int id, String type){
+        this.studioId=id;
+        this.studioType=type;
+    }
     public int getStudioID() {
         return studioID;
     }
     public void setStudioID(int studioID) {
         this.studioID = studioID;
-    }
-    public String getLocation() {
-        return location;
-    }
-    public void setLocation(String location) {
-        this.location = location;
-    }
-    public String getStudioName() {
-        return studioName;
-    }
-    public void setStudioName(String studioName) {
-        this.studioName = studioName;
     }
     public String getStudioType() {
         return studioType;
