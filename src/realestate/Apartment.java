1809d040c349570839d99cf8f430e74909d017d4
Badr-ech <166541769+Badr-ech@users.noreply.github.com>
2024-04-16 12:16:44 +0100

    Update Apartment.java
    added constructor
    removed location and apartment name attribute

--- src/realestate/Apartment.java
+++ src/realestate/Apartment.java
@@ -2,19 +2,14 @@ package realestate;
 
 public class Apartment{
     private int apartmentID;
-    private String location;
-    private String ApartmentName;
+    public Apartment(int id){
+        this.apartmentID=id;
+    }
     public int getApartmentID() {
         return apartmentID;
     }
     public void setApartmentID(int apartmentID) {
         this.apartmentID = apartmentID;
-    }
-    public String getLocation() {
-        return location;
-    }
-    public void setLocation(String location) {
-        this.location = location;
     }
     public String getApartmentName() {
         return ApartmentName;
