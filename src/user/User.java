636a34e5e3c39672c8980e1458b15334bfa2bac2
hileeeehi <97041233+hileeeehi@users.noreply.github.com>
2024-04-19 03:17:06 +0200

    Update User.java

--- src/user/User.java
+++ src/user/User.java
@@ -6,6 +6,16 @@ public class User {
     private int age;
     private String email;
     private String phone;
+    
+    // constructor 
+    public User(int id, String name, int age, String email, String phone){
+        this.id = id; 
+        this.name = name;
+        this.age = age; 
+        this.email = email;
+        this.phone = phone;
+    }
+    //getters and setters methods for existing attributes 
     public int getId() {
         return id;
     }
@@ -36,4 +46,44 @@ public class User {
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
+    //methods
+    public void viewProfile(){
+        System.out.println("Name: ", +name);
+        System.out.println("Age: ", + age);
+        System.out.println("Email: ", +email);
+        System.out.println("Phone: ", +Phone);
+    }
+
+    public void updateProfile(){
+    }
+
+    public void changeProfilePicture(){
+    }
+
+    public void deleteAccount(){
+    }
+
+    public void sendMessage(String message){
+    }
+
+    public void retrieveUserInfo(){
+    }
+
+    public List<RealEstate> browseProperties(){
+        return realEstate; 
+    }
+
+    public void viewPropertyDetails(int propertyID){
+    }
+
+    public void bookmarkProperty(int propertyID){
+    }
+
+    public void contactOwner(int propertyID, String message){
+    }
+
+    public void addComment(Comment comment){
+    }
+
 }
