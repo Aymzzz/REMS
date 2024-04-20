9461b2d9018bd06c8a2d05347194bc85172d2aac
SaadMor <167303882+SaadMor@users.noreply.github.com>
2024-04-20 12:13:12 +0100

    Added constructor and method
    Constructor to initialize owner and method to add real estate to the owner´s list

--- src/user/Owner.java
+++ src/user/Owner.java
@@ -7,7 +7,19 @@ import realestate.RealEstate;
 public class Owner extends User {
     private List<RealEstate> ownedRealEstate;
 
+     public Owner(int id, String name, int age, String email, String phone) {
+        super(id, name, age, email, phone);
+        this.ownedRealEstate = new ArrayList<>();  
+    }
+
+    
     public List<RealEstate> getOwnedRealEstate() {
         return ownedRealEstate;
+    }
+
+    public void addRealEstate(RealEstate realEstate) {
+        if (realEstate != null) {
+            ownedRealEstate.add(realEstate);
+        }
     }
 }
