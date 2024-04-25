4d8d0c0380be089301e5d9e211b230eacbb29159
SaadMor <167303882+SaadMor@users.noreply.github.com>
2024-04-25 22:58:22 +0100

    Update Contractor.java

--- src/user/Contractor.java
+++ src/user/Contractor.java
@@ -8,20 +8,18 @@ public class Contractor extends User {
     
     }
 
-    // Method for a contractor to place a bid on a project
+    
     public void bidForProject(String projectDetails, double bidAmount) {
         System.out.println("Bid placed by Contractor " + getName() + " for project '" + projectDetails + "' with amount: $" + bidAmount);
     }
     
-    // Method for a contractor to update the status of a project
     public void updateWorkStatus(String projectDetails, String status) {
         System.out.println("Update from Contractor " + getName() + " for project '" + projectDetails + "': Status - " + status);
     }
     
-    // Method for listing services provided by the contractor
+
     public void listServices() {
         System.out.println("Listing services provided by Contractor " + getName());
-        // Example services - this would usually be dynamic, possibly loaded from a database
         System.out.println("- Plumbing");
         System.out.println("- Electrical");
         System.out.println("- Remodeling");
