81ae9b89693d41ba2f66a388d4e31b4228512980
SaadMor <167303882+SaadMor@users.noreply.github.com>
2024-04-25 22:57:33 +0100

    Update Contractor.java

--- src/user/Contractor.java
+++ src/user/Contractor.java
@@ -2,9 +2,28 @@ package user;
 
 public class Contractor extends User {
 
+    // Auto-generated constructor stub
     public Contractor(int id, String name, int age, String email, String phone) {
-        super(id, name, age, email, phone);
-        //TODO Auto-generated constructor stub
+        super(id, name, age, email, phone); // Call to the superclass (User) constructor
+    
     }
 
+    // Method for a contractor to place a bid on a project
+    public void bidForProject(String projectDetails, double bidAmount) {
+        System.out.println("Bid placed by Contractor " + getName() + " for project '" + projectDetails + "' with amount: $" + bidAmount);
+    }
+    
+    // Method for a contractor to update the status of a project
+    public void updateWorkStatus(String projectDetails, String status) {
+        System.out.println("Update from Contractor " + getName() + " for project '" + projectDetails + "': Status - " + status);
+    }
+    
+    // Method for listing services provided by the contractor
+    public void listServices() {
+        System.out.println("Listing services provided by Contractor " + getName());
+        // Example services - this would usually be dynamic, possibly loaded from a database
+        System.out.println("- Plumbing");
+        System.out.println("- Electrical");
+        System.out.println("- Remodeling");
+    }
 }
