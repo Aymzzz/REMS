5716c5e68de447a64ae2d20971884ef6359457e7
SaadMor <167303882+SaadMor@users.noreply.github.com>
2024-04-25 22:21:11 +0100

    Update HouseKeeper.java

--- src/user/HouseKeeper.java
+++ src/user/HouseKeeper.java
@@ -1,6 +1,36 @@
 package user;
 
-public class HouseKeeper {
-    private Manager manager;
+import realestate.RealEstate;
 
-}
+public class HouseKeeper extends User {
+    private Manager manager; 
+    private RealEstate assignedBuilding; 
+
+    public HouseKeeper(int id, String name, int age, String email, String phone, Manager manager) {
+        super(id, name, age, email, phone); 
+    }
+
+    
+    public Manager getManager() {
+        return manager;
+    }
+
+   
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+    public RealEstate getAssignedBuilding() {
+        return assignedBuilding;
+    }
+
+   
+    public void setAssignedBuilding(RealEstate assignedBuilding) {
+        if (this.manager != null && this.manager.getRealEstates().contains(assignedBuilding)) {
+            this.assignedBuilding = assignedBuilding;
+            System.out.println("Housekeeper " + this.getName() + " is now assigned to " + assignedBuilding.getName());
+        } else {
+            System.out.println("This housekeeper cannot be assigned to the building as it is not managed by their manager.");
+        }
+    }
+
+   
