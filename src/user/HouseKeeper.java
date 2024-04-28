a663c6463e509c823c6b6bbc4fe9fb4bfac04f92
SaadMor <167303882+SaadMor@users.noreply.github.com>
2024-04-28 19:01:43 +0100

    Errors correction

--- src/user/HouseKeeper.java
+++ src/user/HouseKeeper.java
@@ -3,27 +3,26 @@ package user;
 import realestate.RealEstate;
 
 public class HouseKeeper extends User {
-    private Manager manager; 
-    private RealEstate assignedBuilding; 
+    private Manager manager;
+    private RealEstate assignedBuilding;
 
     public HouseKeeper(int id, String name, int age, String email, String phone, Manager manager) {
-        super(id, name, age, email, phone); 
+        super(id, name, age, email, phone);
+        this.manager = manager;
     }
 
-    
     public Manager getManager() {
         return manager;
     }
 
-   
     public void setManager(Manager manager) {
         this.manager = manager;
     }
+
     public RealEstate getAssignedBuilding() {
         return assignedBuilding;
     }
 
-   
     public void setAssignedBuilding(RealEstate assignedBuilding) {
         if (this.manager != null && this.manager.getRealEstates().contains(assignedBuilding)) {
             this.assignedBuilding = assignedBuilding;
@@ -33,15 +32,11 @@ public class HouseKeeper extends User {
         }
     }
 
-  
-    // method to clean the assigned property
-     public void cleanProperty() {
+    public void cleanProperty() {
         if (assignedBuilding != null) {
             System.out.println("Cleaning property: " + assignedBuilding.getName());
         } else {
             System.out.println("No property assigned for cleaning.");
         }
     }
-
-
-   
+}
