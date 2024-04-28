e9864232a79121b4527aef3ada4e29f323ab07b9
Badr-ech <166541769+Badr-ech@users.noreply.github.com>
2024-04-28 23:29:37 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,22 +1,51 @@
+package com.mycompany.app;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import realestate.*;
 import user.*;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ *
+ * @author Badr
+ */
 public class App {
     private static ArrayList<Account> accounts = new ArrayList<>();
     private static User currentUser;
-    
-    private static ArrayList<RealEstate> realEstates = null;
-    public static void main(String[] args) {
+    private static ArrayList<User> users = new ArrayList<>();
+    private static ArrayList<RealEstate> realEstates = new ArrayList<>();
+
+    /**
+     *
+     * @param args
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        try {
+            realEstates= RealEstateAgency.loadRealEstates();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            users= RealEstateAgency.loadUsers();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         Scanner scanner = new Scanner(System.in);
         Account currentAccount = null;
-        boolean exit = false;
         int choice;
+        boolean exit = false;
         while (!exit) {
-            if (currentAccount == null){
+            while(currentAccount == null){
             System.out.println("\n===== Real Estate Management System Menu =====");
             System.out.println("1. Login");
             System.out.println("2. Sign Up");
@@ -27,6 +56,7 @@ public class App {
             
             try {
                 choice = scanner.nextInt();
+                scanner.nextLine();//Consume the newline character
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input. Please enter a number.");
                 continue;
@@ -37,8 +67,9 @@ public class App {
                         String Username = scanner.nextLine();
                         System.out.print("Enter password: ");
                         String Password = scanner.nextLine();
+                        currentAccount = new Account("0", "0"); // Create a new Account object
                         currentAccount.login(Username, Password);
-                        currentUser = new User(0, null, 0, null, null, currentAccount);
+                        currentUser = new User(0, "default", 0, "default", "default", currentAccount);
                         break;
                     case 2:
                     if (currentUser != null) {
@@ -50,21 +81,26 @@ public class App {
                         System.out.print("Enter password: ");
                         String regPassword = scanner.nextLine();
                         currentAccount.signUp(regUsername, regPassword);
-                        accounts.add(currentAccount);
-                        currentUser = new User(0, null, 0, null, null, currentAccount);
+                        currentUser = new User(0, "default", 0, "default", "default", currentAccount);
+                        users.add(currentUser);
+                       
+                try {
+                    RealEstateAgency.saveUsers();
+                } catch (IOException ex) {
+                    Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                }
                         
                     }
                     break;
                 case 3:
-                    exit = true;
                     System.out.println("Exiting Real Estate Management System. Goodbye!");
-                    break;
+                    System.exit(0);
                 default:
                     System.out.println("Invalid choice. Please try again.");
                     break;
             }
             }
-            else {
+            
                 boolean exit1=false;
                 while(!exit1){
                 System.out.println("\n===== Real Estate Management System Menu =====");
@@ -76,6 +112,7 @@ public class App {
             
             try {
                 choice = scanner.nextInt();
+                scanner.nextLine();//Consume the newline character
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input. Please enter a number.");
                 continue;
@@ -88,13 +125,20 @@ public class App {
                     double minPrice = scanner.nextDouble();
                     System.out.println("Please enter your maximum price.\n Type 0 if you prefer not to specify:");
                     double maxPrice = scanner.nextDouble();
+                    scanner.nextLine();
                     System.out.println("Please enter your desired location.\nType 0 if you prefer not to specify:");
                     String location = scanner.nextLine();
                     ArrayList<RealEstate> foundRealEstates = RealEstateAgency.search(realEstates, name, minPrice, maxPrice, location);
-                    System.out.println("Do you want to sort them by price?\nType 1 if yes, 0 if no:");
-                    int sortingchoice = scanner.nextInt();
-                    if(sortingchoice==1) RealEstateAgency.sortRealEstates(foundRealEstates);
-                    RealEstateAgency.displayRealEstates(foundRealEstates);
+                    if(foundRealEstates!= null) {
+                        System.out.println("Do you want to sort them by price?\nType 1 if yes, 0 if no:");
+                        int sortingchoice = scanner.nextInt();
+                        scanner.nextLine();//Consume the newline character
+                        if(sortingchoice==1) RealEstateAgency.sortRealEstates(foundRealEstates);
+                        RealEstateAgency.displayRealEstates(foundRealEstates);
+                    } else {
+                        System.out.println("No properties found matching your search criteria.");
+                    }
+                    break;
                 case 2:
                     if(currentUser instanceof Manager){
                         boolean exit2=false;
@@ -108,6 +152,7 @@ public class App {
             
                         try {
                             choice = scanner.nextInt();
+                            scanner.nextLine();//Consume the newline character
                         } catch (InputMismatchException e) {
                             System.out.println("Invalid input. Please enter a number.");
                             continue;
@@ -122,6 +167,12 @@ public class App {
                                 location = scanner.nextLine();
                                 RealEstate r = new RealEstate(name, location, price);
                                 RealEstateAgency.addRealEstate(r);
+                                try {
+                                    RealEstateAgency.saveRealEstates();
+                                } catch (IOException ex) {
+                                    Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                                break;
                             case 2: 
                                 System.out.println("Please enter the name of the property you want to remove.\n");
                                 name = scanner.nextLine();
@@ -131,6 +182,7 @@ public class App {
                                 location = scanner.nextLine();
                                 r = new RealEstate(name, location, price);
                                 RealEstateAgency.removeRealEstate(r);
+                                break;
                             case 3:
                                 exit2=true;
                                 break;
@@ -143,8 +195,9 @@ public class App {
                     else{
                         System.out.println("Access denied!!!");
                     }
-                    
+                    break;
                 case 3:
+                    exit=true;
                     exit1=true;
                     break;
                 default:
@@ -153,53 +206,57 @@ public class App {
                 
            }
         }
-        scanner.close();
-                                
         
-        // // This part was done by SBAI Aymane to test my part of the project. - DISREGARD UNLESS TRYING TO CHECK THE VALIDITY OF THE CODE!
-        // Comment comment1 = new Comment(1, 1, 1, "This is an interesting property!");
-        // Comment comment2 = new Comment(2, 2, 2, "I agree, it's perfect for my needs!");
-        // Comment comment3 = new Comment(3, 3, 3, "Nah mid");
+        }                       
+        
+        
+        /*     // This part was done by SBAI Aymane to test my part of the project.
+        Comment comment1 = new Comment(1, 1, 1, "This is an interesting property!");
+        Comment comment2 = new Comment(2, 2, 2, "I agree, it's perfect for my needs!");
+        Comment comment3 = new Comment(3, 3, 3, "Nah mid");
 
-        // // creating a new BuyNow sale type
-        // BuyNow buyNowSaleType = new BuyNow(100000.0f, new user.Buyer(0, "name", 50, null, null, 0, null));
+        // creating a new BuyNow sale type
+        BuyNow buyNowSaleType = new BuyNow(100000.0f, new user.Buyer(0, "name", 50, null, null, 0, null));
 
-        // //posting a comment on the property
-        // comment1.postComment("Welcome to the property discussion");
+        //posting a comment on the property
+        comment1.postComment("Welcome to the property discussion");
 
-        // //replying to the comment
-        // comment2.reply(comment1, "Thanks for sharing your thoughts man");
-        // comment3.reply(comment2, "You're welcome!");
+        //replying to the comment
+        comment2.reply(comment1, "Thanks for sharing your thoughts man");
+        comment3.reply(comment2, "You're welcome!");
 
-        // //Prints the comments and replies
-        // System.out.println("Comment 1: " + comment1.getContent());
-        // System.out.println("Comment 2: " + comment2.getContent());
-        // System.out.println("Comment 3: " + comment3.getContent());
+        //Prints the comments and replies
+        System.out.println("Comment 1: " + comment1.getContent());
+        System.out.println("Comment 2: " + comment2.getContent());
+        System.out.println("Comment 3: " + comment3.getContent());
 
-        // //Printing  the replies
-        // System.out.println("Replies to Comment 1:");
-        // for (Comment reply : comment1.getReplies()) {
-        //     System.out.println("- " + reply.getContent());
-        // }
-
-        // //Printing the replies to the replies - using the collections
-        // System.out.println("Replies to Comment 2:");
-        // for (Comment reply : comment2.getReplies()) {
-        //     System.out.println("- " + reply.getContent());
-        // }
-
-        // //Printing the replies to the replies to the replies
-        // System.out.println("Replies to Comment 3:");
-        // for (Comment reply : comment3.getReplies()) {
-        //     System.out.println("- " + reply.getContent());
-        // }
-
-        // //prints the BuyNow sale type details for better understanding
-        // System.out.println("BuyNow Sale Type Details:");
-        // System.out.println("Price: " + buyNowSaleType.getPrice());
-        // System.out.println("Buyer: " + buyNowSaleType.getBuyer().getName());
-        // System.out.println("Buyer's Email: " + buyNowSaleType.getBuyer().getEmail());
-    }
-            }
+        //Printing  the replies
+        System.out.println("Replies to Comment 1:");
+        for (Comment reply : comment1.getReplies()) {
+            System.out.println("- " + reply.getContent());
         }
+
+        //Printing the replies to the replies - using the collections
+        System.out.println("Replies to Comment 2:");
+        for (Comment reply : comment2.getReplies()) {
+            System.out.println("- " + reply.getContent());
+        }
+
+        //Printing the replies to the replies to the replies
+        System.out.println("Replies to Comment 3:");
+        for (Comment reply : comment3.getReplies()) {
+            System.out.println("- " + reply.getContent());
+        }
+
+        //prints the BuyNow sale type details for better understanding
+        System.out.println("BuyNow Sale Type Details:");
+        System.out.println("Price: " + buyNowSaleType.getPrice());
+        System.out.println("Buyer: " + buyNowSaleType.getBuyer().getName());
+        System.out.println("Buyer's Email: " + buyNowSaleType.getBuyer().getEmail());
+    
+         
+         */
+    System.exit(0);
     }
+}
+  
