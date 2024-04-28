ebd13719fc60ca9b479ca2c9d316b57b10a4a9a5
SaadMor <167303882+SaadMor@users.noreply.github.com>
2024-04-28 19:15:06 +0100

    Errors fixing

--- src/user/Owner.java
+++ src/user/Owner.java
@@ -2,28 +2,21 @@ package user;
 
 import java.util.List;
 import java.util.ArrayList;
-
-
 import realestate.RealEstate;
 
 public class Owner extends User {
     private List<RealEstate> ownedRealEstate;
 
-     public Owner(int id, String name, int age, String email, String phone) {
+    public Owner(int id, String name, int age, String email, String phone) {
         super(id, name, age, email, phone);
-        this.ownedRealEstate = new ArrayList<>();  
+        this.ownedRealEstate = new ArrayList<>();
     }
 
-    
-    
-    /** 
-     * @return List<RealEstate>
-     */
     public List<RealEstate> getOwnedRealEstate() {
         return ownedRealEstate;
     }
 
-   public void addRealEstate(RealEstate realEstate) {
+    public void addRealEstate(RealEstate realEstate) {
         if (realEstate != null && !ownedRealEstate.contains(realEstate)) {
             ownedRealEstate.add(realEstate);
             System.out.println("Real estate added to your properties.");
